1eff3fe71353c649d337b6302eff14d1b661eb81
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-06-12 10:30:41 -0300

    feat: add tfc data module and dataset

--- data_modules/har.py
+++ data_modules/har.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append("../")
+
 import os
 import urllib.request
 import zipfile
@@ -6,7 +10,10 @@ from typing import List, Tuple, Union
 import lightning as L
 import numpy as np
 import pandas as pd
+import torch
 from torch.utils.data import DataLoader, Dataset
+from transforms.time_series import DataTransform_FD, DataTransform_TD
+import torch.fft as fft
 
 
 class HarDataset(Dataset):
@@ -40,11 +47,11 @@ class HarDataset(Dataset):
         | 0.498217  | 0.00001   | 1.414141  | 3.141592  |  2     |
         +-----------+-----------+-----------+-----------+--------+
 
-        The ``feature_column_prefixes`` parameter is used to select the columns 
+        The ``feature_column_prefixes`` parameter is used to select the columns
         that will be used as features. For instance, if we want to use only the
         accel-x series, we can set ``feature_prefixes=["accel-x"]``. If we want
         to use both accel-x and accel-y, we can set
-        ``feature_prefixes=["accel-x", "accel-y"]``. 
+        ``feature_prefixes=["accel-x", "accel-y"]``.
         The label column is specified by the ``target_column`` parameter.
 
         The dataset will return a 2-element tuple with the data and the label.
@@ -67,11 +74,11 @@ class HarDataset(Dataset):
         flatten : bool, optional
             If False, the data will be returned as a vector of shape (C, T),
             else the data will be returned as a vector of shape  T*C.
-            
+
         Examples
         --------
         # supposing our data.csv file contains the following data:
-        
+
         +-----------+-----------+-----------+-----------+--------+
         | accel-x-0 | accel-x-1 | accel-y-0 | accel-y-1 |  class |
         +-----------+-----------+-----------+-----------+--------+
@@ -79,7 +86,7 @@ class HarDataset(Dataset):
         | 0.6820123 | 0.02123   | 0.502123  | 0.502123  |  1     |
         | 0.498217  | 0.00001   | 1.414141  | 3.141592  |  2     |
         +-----------+-----------+-----------+-----------+--------+
-        
+
         # Using the data from data.csv, and flatten=True
         >>> data_path = "data.csv"
         >>> dataset = HarDataset(
@@ -111,7 +118,6 @@ class HarDataset(Dataset):
         self.feature_column_prefixes = feature_column_prefixes
         self.target_column = target_column
         self.flatten = flatten
-
         # Read data
         self.data = pd.read_csv(self.csv_file_path)
         # List of list of columns to select, based on their prefixes
@@ -128,7 +134,7 @@ class HarDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx: int ) -> Tuple[np.ndarray, int]:
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, int]:
         """Retuns a 2-element tuple with the data and the label.
 
         Parameters
@@ -141,18 +147,16 @@ class HarDataset(Dataset):
         Tuple[np.ndarray, int]
             The data and the label of the sample at index `idx`
         """
-        
+
         data = np.zeros(
             shape=(len(self.columns_to_select), len(self.columns_to_select[0])),
             dtype=np.float32,
         )
         for channel in range(len(self.columns_to_select)):
-            data[channel, :] = self.data.iloc[idx][
-                self.columns_to_select[channel]
-            ]
+            data[channel, :] = self.data.iloc[idx][self.columns_to_select[channel]]
         if self.flatten:
             data = data.flatten()
-        
+
         target = self.data.iloc[idx][self.target_column]
         return data, target
 
@@ -179,20 +183,20 @@ class HarDataModule(L.LightningDataModule):
         """Encapsulates the data loading and processing for the HAR dataset.
         This class is a subclass of `LightningDataModule` and implements the
         `train_dataloader`, `val_dataloader` and `test_dataloader` methods.
-        
+
         The dataset is assumed to be stored in a folder with the following
         structure:
-            
+
             ```
             root_data_dir
             ├── train.csv
             ├── validation.csv
             └── test.csv
             ```
-            
+
         The data is assumed to be stored in CSV files with the following
         structure:
-        
+
         +-----------+-----------+-----------+-----------+--------+
         | accel-x-0 | accel-x-1 | accel-y-0 | accel-y-1 |  class |
         +-----------+-----------+-----------+-----------+--------+
@@ -200,24 +204,24 @@ class HarDataModule(L.LightningDataModule):
         | 0.6820123 | 0.02123   | 0.502123  | 0.502123  |  1     |
         | 0.498217  | 0.00001   | 1.414141  | 3.141592  |  2     |
         +-----------+-----------+-----------+-----------+--------+
-        
+
         `train_dataloader` method will create a `HarDataset` object with the
         `train.csv` file, shuffle the data and return a DataLoader object.
-        
+
         `val_dataloader` method will create a `HarDataset` object with the
         `validation.csv` file, do not shuffle the data and return a DataLoader
-        
+
         `test_dataloader` method will create a `HarDataset` object with the
-        `test.csv` file, do not shuffle the data and return a DataLoader        
+        `test.csv` file, do not shuffle the data and return a DataLoader
 
         Parameters
         ----------
         root_data_dir : Union[Path, str]
-            The path to the folder with "train.csv", "validation.csv" and 
+            The path to the folder with "train.csv", "validation.csv" and
             "test.csv" files inside it.
         feature_column_prefixes : list, optional
             The prefix of the column names in the dataframe that will be used
-            to become features. This parameter is used for instantiating the 
+            to become features. This parameter is used for instantiating the
             `HarDataset` object.
         target_column : str, optional
             The name of the column that will be used as label. This parameter
@@ -227,7 +231,7 @@ class HarDataModule(L.LightningDataModule):
             else the data will be returned as a vector of shape  T*C. This
             parameter is used for instantiating the `HarDataset` object.
         batch_size : int, optional
-            Number of samples per batch. This parameter is used for 
+            Number of samples per batch. This parameter is used for
             instantiating the DataLoader objects.
         """
         super().__init__()
@@ -241,15 +245,14 @@ class HarDataModule(L.LightningDataModule):
             "train": os.path.join(self.root_data_dir, "train.csv"),
             "validation": os.path.join(self.root_data_dir, "validation.csv"),
             "test": os.path.join(self.root_data_dir, "test.csv"),
-
         }
         self.setup()
 
-    def setup(self, stage:str = None) -> None:
+    def setup(self, stage: str = None) -> None:
         # Verify that the data is available. If not, fectch and unzip dataset
         for k, v in self.csv_files.items():
             if not os.path.exists(v):
-                print(v,"file is missing")
+                print(v, "file is missing")
                 self.fetch_and_unzip_dataset()
 
     def fetch_and_unzip_dataset(self) -> None:
@@ -268,9 +271,10 @@ class HarDataModule(L.LightningDataModule):
         with zipfile.ZipFile(self.zip_file, "r") as zip_ref:
             zip_ref.extractall(self.root_data_dir)
         print("Data downloaded and extracted")
-        
 
-    def _get_dataset_dataloader(self, path: Path, shuffle: bool) -> DataLoader[HarDataset]:
+    def _get_dataset_dataloader(
+        self, path: Path, shuffle: bool
+    ) -> DataLoader[HarDataset]:
         """Create a HarDataset object and return a DataLoader object.
 
         Parameters
@@ -298,22 +302,126 @@ class HarDataModule(L.LightningDataModule):
             num_workers=1,
         )
         return dataloader
-    
+
     def train_dataloader(self):
         dataloader = self._get_dataset_dataloader(
             self.root_data_dir / "train.csv", shuffle=True
         )
         return dataloader
-    
+
     def val_dataloader(self):
         dataloader = self._get_dataset_dataloader(
             self.root_data_dir / "validation.csv", shuffle=False
         )
         return dataloader
-    
+
     def test_dataloader(self):
         dataloader = self._get_dataset_dataloader(
             self.root_data_dir / "test.csv", shuffle=False
         )
         return dataloader
 
+
+class TFCDataModule(L.LightningDataModule):
+    def __init__(self, root_data_dir: Union[Path, str], batch_size: int = 32):
+        self.root_data_dir = Path(root_data_dir)
+        self.batch_size = batch_size
+
+    def _get_dataset_dataloader(self, path: Path, shuffle: bool):
+        dataset = TFCHarDataset(
+            path,
+        )
+
+        dataloader = DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=shuffle,
+            num_workers=1,
+        )
+        return dataloader
+
+    def train_dataloader(self):
+        dataloader = self._get_dataset_dataloader(
+            self.root_data_dir / "train.csv", shuffle=True
+        )
+        return dataloader
+
+    def val_dataloader(self):
+        dataloader = self._get_dataset_dataloader(
+            self.root_data_dir / "validation.csv", shuffle=False
+        )
+        return dataloader
+
+    def test_dataloader(self):
+        dataloader = self._get_dataset_dataloader(
+            self.root_data_dir / "test.csv", shuffle=False
+        )
+        return dataloader
+
+
+class TFCHarDataset(Dataset):
+    def __init__(
+        self,
+        csv_file_path: Union[Path, str],
+        feature_column_prefixes: list = (
+            "accel-x",
+            "accel-y",
+            "accel-z",
+            "gyro-x",
+            "gyro-y",
+            "gyro-z",
+        ),
+        target_column: str = "standard activity code",
+        training_mode: str = "pre_train",
+    ):
+        super(TFHarDataset, self).__init__()
+        self.csv_file_path = Path(csv_file_path)
+        data_df = pd.read_csv(self.csv_file_path)
+        self.len = len(data_df)
+        self.feature_column_prefixes = feature_column_prefixes
+        self.training_mode = training_mode
+        self.columns_to_select = [
+            self._list_columns_starting_with(data_df, prefix)
+            for prefix in self.feature_column_prefixes
+        ]
+
+        all_columns = sum(self.columns_to_select, [])
+        self.data = data_df[all_columns].values
+        self.data = self.data.reshape(
+            -1, len(self.columns_to_select), len(self.columns_to_select[0])
+        )
+
+        self.x_data = torch.tensor(self.data[:, :, :])
+        self.x_data_f = fft.fft(self.x_data).abs()
+        self.y_data = torch.tensor(data_df[target_column].values)
+
+        if (
+            training_mode == "pre_train"
+        ): 
+            self.aug1 = DataTransform_TD(self.x_data)
+            self.aug1_f = DataTransform_FD(self.x_data_f)
+
+    @staticmethod
+    def _list_columns_starting_with(df: pd.DataFrame, prefix: str) -> List[str]:
+        return [column for column in df.columns if column.startswith(prefix)]
+
+    def __getitem__(self, index):
+        if self.training_mode == "pre_train":
+            return (
+                self.x_data[index],
+                self.y_data[index],
+                self.aug1[index],
+                self.x_data_f[index],
+                self.aug1_f[index],
+            )
+        else:
+            return (
+                self.x_data[index],
+                self.y_data[index],
+                self.x_data[index],
+                self.x_data_f[index],
+                self.x_data_f[index],
+            )
+
+    def __len__(self):
+        return self.len
